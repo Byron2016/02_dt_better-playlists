3486e4b01fdbd5aab57cfc53e1476a25223c95b8
MiPersona <bgva2005@yahoo.com>
2020-06-14 18:00:34 -0500

    0005_Despliega fake data con interesante forma de llamarla en módulo principal

--- src/components/pantalla_principal/index.js
+++ src/components/pantalla_principal/index.js
@@ -1,11 +1,54 @@
 import React from 'react';
 import './styles.css';
 
+let fakeServerData = {
+    user: {
+      name: "acuario2020",
+      playlists: [
+        {
+          name: "My favorites",
+          songs: [
+            { name: "Beat It", duration: 1345 },
+            { name: "Cannelloni Makaroni", duration: 1236 },
+            { name: "Rosa helikopter", duration: 70000 },
+          ]
+        },
+  
+        {
+          name: "Discover Weekly",
+          songs: [
+            { name: "Beat It", duration: 1345 },
+            { name: "Cannelloni Makaroni", duration: 1236 },
+            { name: "Rosa helikopter", duration: 70000 },
+          ]
+        },
+  
+        {
+          name: "Another playlist - the best!",
+          songs: [
+            { name: "Beat It", duration: 1345 },
+            { name: "Cannelloni Makaroni", duration: 1236 },
+            { name: "Rosa helikopter", duration: 70000 },
+          ]
+        },
+  
+        {
+          name: "Playlist - yeah!",
+          songs: [
+            { name: "Beat It", duration: 1345 },
+            { name: "Cannelloni Makaroni", duration: 1236 },
+            { name: "Rosa helikopter", duration: 70000 },
+          ]
+        },
+      ],
+    },
+  };
+
 class Aggregate extends React.Component {
     render() {
         return (
             <div className="aggregate">
-                <h2 className="aggregate__titulo">Aggregate Number Text</h2>
+                <h2 className="aggregate__titulo">{this.props.playlists && this.props.playlists.length}</h2>
             </div>
         );
     }
@@ -39,11 +82,33 @@ class Playlist extends React.Component {
 }
 
 class PantallaPrincipal extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+          serverData: {},
+          filterString: "",
+        };
+    }
+    
+    componentDidMount() {
+        setTimeout(() => {
+          this.setState({
+            serverData: fakeServerData,
+          });
+        }, 1000);
+    }
+    
     render() {
         return (
             <div className='contendor'>
-                <h1 className="contendor__titulo">Titulo Dentro de pantalla_principal</h1>
-                <Aggregate />
+                <h1 className="contendor__titulo">{
+                    this.state.serverData.user &&
+                    this.state.serverData.user.name}'s PlayList
+                </h1>
+                <Aggregate playlists={
+                    this.state.serverData.user &&
+                    this.state.serverData.user.playlists} />
                 <Aggregate />
                 <Filter />
                 <Playlist />
